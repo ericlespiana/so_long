f5b9cca9743a9670a7e4004ae6e0d168f05009db
erpiana <erpiana@student.42sp.org.br>
2024-02-23 09:50:59 -0300

    Teste da primeira e ultima linha implementado e de proporção. Leak de 1 byte

--- include/ft_utils_error.h
+++ include/ft_utils_error.h
@@ -6,7 +6,7 @@
 /*   By: erpiana <erpiana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 07:51:11 by erpiana           #+#    #+#             */
-/*   Updated: 2024/02/23 08:05:06 by erpiana          ###   ########.fr       */
+/*   Updated: 2024/02/23 08:53:45 by erpiana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # define READ_ERROR "Error\nIt was no possible to read the file descriptor\n"
 # define JOIN_ERROR "Error\nCan't join strings\n"
 # define ERROR_COLUMN "Error\nColumn don't have the same proportion\n"
+# define ERROR_WALL "Error\nMap don't surrounded by 1\n"
 
 void	ft_error(char *msg);
 
--- srcs/ft_validate.c
+++ srcs/ft_validate.c
@@ -6,37 +6,56 @@
 /*   By: erpiana <erpiana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 07:37:40 by erpiana           #+#    #+#             */
-/*   Updated: 2024/02/23 08:44:08 by erpiana          ###   ########.fr       */
+/*   Updated: 2024/02/23 09:49:28 by erpiana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_so_long.h"
+
+static void	check_one(char *temp)
+{
+	int	i;
+
+	i = 0;
+	while (temp[i] && temp[i] == '1')
+		i++;
+	if (temp[i] != '\n' && temp[i] != '\0')
+	{
+		free(temp);
+		ft_error(ERROR_WALL);
+	}
+}
 
 static void	ft_validate_proportion(int fd, t_map *map)
 {
 	char	*temp;
 	char	*buffer;
 	size_t	column_size;
-	
+
 	(void)map;
 	temp = get_next_line(fd);
 	if (!temp)
 		ft_error(READ_ERROR);
 	column_size = ft_strlen(temp);
-	buffer = ft_strdup("");
+	buffer = NULL;
+	check_one(temp);
 	while (temp)
 	{
+		if (!ft_strchr(temp, '\n'))
+		{
+			column_size--;
+			check_one(temp);
+		}
 		if (ft_strlen(temp) != column_size)
 		{
 			free(buffer);
 			free(temp);
 			ft_error(ERROR_COLUMN);
 		}
-		buffer = ft_strjoin(buffer, temp);
+		buffer = ft_strjoin_gnl(buffer, temp);
 		free(temp);
 		temp = get_next_line(fd);
 	}
-	ft_printf("%s", buffer);
 }
 
 void	ft_validate(char *map_name)

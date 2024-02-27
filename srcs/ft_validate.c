a4b24baa050c295682cafa1fa47e54ebb709bfbd
erpiana <erpiana@student.42sp.org.br>
2024-02-27 14:16:18 -0300

    fix: line's proportion validation fixed and no leaks

--- srcs/ft_validate.c
+++ srcs/ft_validate.c
@@ -6,13 +6,13 @@
 /*   By: erpiana <erpiana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 07:37:40 by erpiana           #+#    #+#             */
-/*   Updated: 2024/02/27 10:22:43 by erpiana          ###   ########.fr       */
+/*   Updated: 2024/02/27 14:15:11 by erpiana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_so_long.h"
 
-static void	ft_validate_proportion(int fd, t_map *map);
+static void	ft_validate_proportion(t_map *map, char *temp);
 static void	check_one_and_size(char *tmp, t_map *map, char *buf, size_t c_size);
 
 static void	init_variable_map(t_map *map)
@@ -22,41 +22,51 @@ static void	init_variable_map(t_map *map)
 	map->collectibles = 0;
 }
 
-//Funcão principal desse arquivo
+static void	free_error(t_map *map, char *tmp, char *buf)
+{
+	get_next_line(map->fd, 0);
+	free(tmp);
+	if (buf)
+		free(buf);
+	close(map->fd);
+	ft_error("Error\nYour file is not sourroundet by \
+or it is out of proportion!\n");
+}
 
 void	ft_validate(char *map_name)
 {
 	t_map	map;
+	char	*temp;
 
 	init_variable_map(&map);
 	map.fd = open(map_name, O_RDONLY, 0666);
 	if (map.fd == -1)
 		ft_error("Error to open map!\n");
-	ft_validate_proportion(map.fd, &map);
+	temp = get_next_line(map.fd, 1);
+	if (!temp)
+	{
+		close(map.fd);
+		ft_error("Error\nThis file is empty!\n");
+	}
+	ft_validate_proportion(&map, temp);
 	close(map.fd);
 }
 
-static void	ft_validate_proportion(int fd, t_map *map)
+static void	ft_validate_proportion(t_map *map, char *temp)
 {
-	char	*temp;
 	char	*buffer;
-	size_t	column_size;
+	size_t	col_size;
 
-	temp = get_next_line(fd, 1);
-	if (!temp)
-		ft_error("Error\nIt was no possible to read the file descriptor\n");
 	buffer = NULL;
-	column_size = ft_strlen(temp);
-	check_one_and_size(temp, map, buffer, column_size);
+	col_size = ft_strlen(temp);
 	while (temp)
 	{
 		if (!ft_strchr(temp, '\n'))
-			check_one_and_size(temp, map, buffer, --column_size);
-		if (ft_strlen(temp) != column_size)
-			check_one_and_size(temp, map, buffer, column_size);
+			--col_size;
+		check_one_and_size(temp, map, buffer, col_size);
 		buffer = ft_strjoin_gnl(buffer, temp);
 		free(temp);
-		temp = get_next_line(fd, 1);
+		temp = get_next_line(map->fd, 1);
 	}
 	free(temp);
 	free(buffer);
@@ -64,18 +74,21 @@ static void	ft_validate_proportion(int fd, t_map *map)
 
 static void	check_one_and_size(char *tmp, t_map *map, char *buf, size_t c_size)
 {
-	int	i;
+	int			i;
+	int			flag;
+	static int	times;
 
 	i = 0;
-	while (tmp[i] && tmp[i] == '1')
-		i++;
-	if ((tmp[i] != '\n' && tmp[i] != '\0') || ft_strlen(tmp) != c_size)
+	flag = 0;
+	if (times == 0 || !ft_strchr(tmp, '\n'))
 	{
-		get_next_line(map->fd, 0);
-		free(tmp);
-		if (buf)
-			free(buf);
-		close(map->fd);
-		ft_error("Error\nMap don't surrounded by 1\n");
+		while (tmp[i] && tmp[i] == '1')
+			i++;
+		if (tmp[i] != '\n' && tmp[i] != '\0')
+			flag = 1;
 	}
+	if (c_size != ft_strlen(tmp))
+		flag = 1;
+	if (flag)
+		free_error(map, tmp, buf);
 }
